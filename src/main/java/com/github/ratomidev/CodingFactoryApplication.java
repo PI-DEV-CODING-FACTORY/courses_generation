acde479406ca29f2bf490cd13fc6047d22af87dc
ramroumette <mr.toumirami@outlook.fr>
2025-04-15 14:50:15 +0100

    refactor: clean up pom.xml and application.properties,  form EURIKA add spacing in CodingFactoryApplication

--- src/main/java/com/github/ratomidev/CodingFactoryApplication.java
+++ src/main/java/com/github/ratomidev/CodingFactoryApplication.java
@@ -3,6 +3,7 @@ package com.github.ratomidev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CodingFactoryApplication {
 
